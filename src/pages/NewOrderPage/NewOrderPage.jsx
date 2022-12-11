fd5fe87ffd60d3d1a54850ff70069ee18a47b026
sammy2342 <sammyfares303@gmail.com>
2022-12-10 23:41:38 -0500

    complete render on page for apy sainty

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,17 +1,20 @@
 import './NewOrderPage.css'
 import { client } from '../../libary/client'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
 export default function NewOrderPage() {
+
+    const [productsInList, setProductsInList] = useState([])
 
     useEffect(function() {
         async function getImage() { 
             try {
                 const query = '*[_type == "product"]'
                 const products = await client.fetch(query)
-                console.log(products)
+                setProductsInList(products)
+                console.log(productsInList, 'this is for the new product')
             } catch(err) {
                 console.log(err)
             }
@@ -21,20 +24,22 @@ export default function NewOrderPage() {
     }, [])
 
     return (
-        <>  
-            <h1>NewOrderPage</h1> 
-            <div className="product-headers-title">
-                <h2>Best Selling Products</h2>
-                <p className='title-header-p-tag'>Speakers of many viriations</p>
-            </div>
+        <>
+            {productsInList ? <>  
+                <h1>NewOrderPage</h1> 
+                <div className="product-headers-title">
+                    <h2>Best Selling Products</h2>
+                    <p className='title-header-p-tag'>Speakers of many viriations</p>
+                </div>
 
-            <div className='product-container-item'>
-                {['Product 1', 'Product 2'].map((item) => { 
-                    return item
-                })}
-            </div>
+                <div className='product-container-item'>
+                    {productsInList.map((item, idx) => (
+                        <div key={idx}>{item.name}</div>
+                    ))}
+                </div>
 
-            Footer
+                Footer
+            </>: null}
         </>
     )
 }
