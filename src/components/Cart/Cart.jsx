270d26e6e4d39136da0a8bb9e4364d112d9ed35d
sammy2342 <sammyfares303@gmail.com>
2022-12-14 01:32:14 -0500

    complete side menu for empty items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,10 @@
 import { useRef } from 'react'
 import './Cart.css'
-import { AiOutlineMinus, AiOutLinePlus, AiOutlineLeft, AiOutLineShopping, TiDeleteOutLine, AiOutlineDelete, AiFillCarryOut, AiOutlineDoubleRight } from 'react-icons/ai'
+import { AiOutlineMinus, AiOutLinePlus, AiOutlineLeft, AiOutlineShopping, TiDeleteOutLine, AiOutlineDelete, AiFillCarryOut, AiOutlineDoubleRight } from 'react-icons/ai'
 import toast from 'react-hot-toast'
 import { useStateContext } from '../../context/StateContext'
 import { urlFor } from '../../libary/client'
+import { Link } from 'react-router-dom'
 
 export default function Cart() {
 
@@ -18,6 +19,18 @@ export default function Cart() {
                     <span className='heading'>Your Cart</span>
                     <span className='cart-num-items'>({totalQuantites} items)</span>
                 </button>
+
+                {cartItems.length < 1 && (
+                    <div className='empty-cart'>
+                        <AiOutlineShopping size={200} />
+                        <h2>Your Shopping bag is empty</h2>
+                        <Link to='/orders/new'>
+                            <button type='button' className='btn' onClick={() => setShowCart(false)}>
+                                Continue Shopping
+                            </button>
+                        </Link>
+                    </div>
+                )}
             </div>
         </div>
     )
