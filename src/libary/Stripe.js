a9f50c73a755036c283ed6970d1fa480f4b01a6d
sammy2342 <sammyfares303@gmail.com>
2022-12-20 17:54:12 -0500

    env

--- src/libary/Stripe.js
+++ src/libary/Stripe.js
@@ -4,8 +4,7 @@ let stripePromise
 
 const getStripe = () => { 
     if(!stripePromise) { 
-        // console.log(process.env.TEST)
-        stripePromise = loadStripe('pk_test_51MFNACBIEZeIAiqxb5EeO8YSkvblEDSNpvpjbzC7I8dbpZ4Vk5OrxWz7eU4CZhCMi0HuFzFKdFZPy1htuXR2dbH400f1xTqxsM')
+        stripePromise = loadStripe(process.env.REACT_APP_API_TEST)
     }
 
     return stripePromise
