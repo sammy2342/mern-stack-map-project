9a387c592f3d3b375895ee75b47c332c5ca9db32
sammy2342 <sammyfares303@gmail.com>
2022-12-27 23:42:52 -0500

    complete auth page

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import './Logo.css';
+
+export default function Logo() {
+  return (
+    <div className="Logoss">
+      <div>Cort</div>
+      <div>Folio</div>
+    </div>
+  );
+}
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,19 @@
-import SignUpForm from '../../components/SignUpForm/SignUpForm'
-import LoginForm from '../../components/LoginForm/LoginForm'
+import { useState } from 'react';
+import './AuthPage.css';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import Logo from '../../components/Logo/Logo';
 
 export default function AuthPage({ setUser }) {
-    return (
-        <main>
-            <h1>AuthPage</h1>
-            <SignUpForm setUser={setUser} />
-            <LoginForm setUser={setUser} />
-        </main>
-    )
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <main className="AuthPage">
+      <div>
+        <Logo />
+        <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+      </div>
+      {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+    </main>
+  );
 }
